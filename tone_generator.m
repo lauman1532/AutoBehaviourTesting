4a0825be8ca089df00fea31bbbe66ebfbbf94235
Vick Lau <lauman1532@users.noreply.github.com>
2018-01-18 09:58:25 +0000

    turns into function

--- tone_generator.m
+++ tone_generator.m
@@ -3,25 +3,24 @@
 %%% Vick Lau
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%function tone_generator
-clear all; close all; clc
+function [y] = tone_generator(duration, toneFreq, amplitude)
 
     fs = 44100;
-    duration = 2;
-    toneFreq = 1000;
-    amplitude = 0.2;
+    %duration = 2;
+    %toneFreq = 1000; % max freq ~ 20kHz
+    %amplitude = 0.2;
     
     t = 0+1/fs:1/fs:duration;
     y = amplitude*cos(2*pi*toneFreq*t);
-    sound(y, fs);
+    
+    %sound(y, fs);
     
     % time-domain
-    figure()
-    plot(t, y)
+    %figure()
+    %plot(t, y)
    
     % freq-domian
-    f_ax = -pi+pi/length(t):2*pi/length(t):pi-pi/length(t);
-    figure()
-    plot(f_ax,fftshift(fft(y)));
-
-%end
+    %f_ax = -pi+pi/length(t):2*pi/length(t):pi-pi/length(t);
+    %figure()
+    %plot(f_ax,fftshift(fft(y)));
+end
